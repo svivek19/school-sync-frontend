c17a4d4ec02d94b954a965d58b811a1fc6f47abd
svivek19 <svivek030503@gmail.com>
2024-08-17 15:39:15 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Pages
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
