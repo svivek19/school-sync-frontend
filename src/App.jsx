2455840a07935d3112e2921cd49b5f337f0100f9
svivek19 <svivek030503@gmail.com>
2024-08-17 14:49:13 +0530

    route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Pages
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
