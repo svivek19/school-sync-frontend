e2a93f3fde734fe431274bdd3f511e7b0b550bd3
svivek19 <svivek030503@gmail.com>
2024-08-17 14:46:47 +0530

    feat: route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Pages
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
